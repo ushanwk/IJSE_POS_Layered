0621206de9d76fd34ad9efdac3074884a544b31a
UshanKaushalya <Ushanwk22@gmail.com>
2023-09-05 10:27:32 +0530

    customer load all complete

--- Back_End/src/lk/ijse/pos/servlet/CustomerServlet.java
+++ Back_End/src/lk/ijse/pos/servlet/CustomerServlet.java
@@ -2,12 +2,14 @@ package lk.ijse.pos.servlet;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
 
 @WebServlet(urlPatterns = {"/customer"})
@@ -15,7 +17,7 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        try {
+        try(PrintWriter writer = resp.getWriter()) {
 
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaeePosApp", "root", "ushan1234");
@@ -26,16 +28,27 @@ public class CustomerServlet extends HttpServlet {
             resp.addHeader("Access-Control-Allow-Origin","*");
             resp.addHeader("Content-Type","application/json");
 
-            JsonArrayBuilder json = Json.createArrayBuilder();
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
 
             while(resultSet.next()){
+
                 String custId = resultSet.getString(1);
                 String custName = resultSet.getString(2);
                 String custAddress = resultSet.getString(3);
                 String custSalary = resultSet.getString(4);
 
-                System.out.println(custId + custName + custAddress + custSalary);
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("id", custId);
+                customer.add("name", custName);
+                customer.add("address", custAddress);
+                customer.add("salary", custSalary);
+
+                allCustomers.add(customer.build());
+
             }
+
+            writer.print(allCustomers.build());
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
--- Front_End/controller/CustomerController.js
+++ Front_End/controller/CustomerController.js
@@ -2,4 +2,25 @@ let link = "http://localhost:8080/Back_End_Web_exploded/customer";
 
 function loadAll(){
 
+    $.ajax({
+
+        url : link,
+        success : function(res){
+            let customers = $(res);
+            $('#tblCustomer').empty();
+
+            for(let i = 0; i < customers.length; i++){
+                let id = customers[i].id;
+                let name = customers[i].name;
+                let address = customers[i].address;
+                let salary = customers[i].salary;
+
+                let row =`<tr><td>${id}</td><td>${name}</td><td>${address}</td><td>${salary}</td></tr>`;
+                $('#tblCustomer').append(row);
+            }
+        }
+    });
+
 }
+
+loadAll();
